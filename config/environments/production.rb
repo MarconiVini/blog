ae72cea538b92fc96b91e203d2a7f2ef20f38e53
Marcos Vinicius Cardoso Santini <cardosounicamp@gmail.com>
2016-02-15 19:51:51 -0200

    added paginator and styles to it

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -71,6 +71,14 @@ Rails.application.configure do
   # the I18n.default_locale when a translation cannot be found).
   config.i18n.fallbacks = true
 
+  config.time_zone = 'Brasilia'
+
+  # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
+  # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
+
+  # Configure the default encoding used in templates for Ruby 1.9.
+  config.encoding = "utf-8"
+
   # Send deprecation notices to registered listeners.
   config.active_support.deprecation = :notify
 
