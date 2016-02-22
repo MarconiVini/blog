91bdad041cec48cc5dc67cb60d0ea3d231a0d737
Marcos Vinicius Cardoso Santini <cardosounicamp@gmail.com>
2016-02-22 13:43:42 -0300

    changed login details

--- app/controllers/admin/sessions_controller.rb
+++ app/controllers/admin/sessions_controller.rb
@@ -16,7 +16,7 @@ class Admin::SessionsController < Devise::SessionsController
 
   def after_sign_out_path_for(resource_or_scope)
     case resource_or_scope
-      when :admin then new_admin_post_path
+      when :admin then new_admin_session_path
     end
   end
 end
