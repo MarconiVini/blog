4d52dffb1536e0a1e91fa8294998f5721e315fa6
Marcos Vinicius Cardoso Santini <cardosounicamp@gmail.com>
2016-02-28 15:41:42 -0300

    get text more readable

--- app/helpers/common/time_helper.rb
+++ app/helpers/common/time_helper.rb
@@ -1,10 +1,15 @@
 module Common::TimeHelper 
   def render_time_published(time)
-    return time_ago_in_words(time) if time > Time.now - 30.days
+    return "#{distance_of_time_in_words_to_now(time)} #{sulfix}" if time > Time.now - 30.days
     time.strftime(get_user_location)
   end
 
   private
+
+  def sulfix
+    I18n.locale == "pt-BR" ? "atrás" : "ago"
+  end
+
   def get_user_location
     I18n.locale == "pt-BR" ? "%d/%m/%y" : "%m/%d/%y"
   end
